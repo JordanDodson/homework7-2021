f961497f34f59a2985bd206f76f4dad546fb2faf
JordanDodson <jonodo2468@gmail.com>
2021-03-31 21:39:25 -0400

    Update video.js

--- js/video.js
+++ js/video.js
@@ -1,11 +1,82 @@
 var video;
+var volume;
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 
+	volume = document.querySelector("#volume");
+	video =document.querySelector("#player1");
+	
 });
 
 // document.querySelector("#play").addEventListener("click", function() {
 // 	console.log("Play Video");
 // });
+
+document.querySelector("#play").addEventListener("click", function() {
+	video.play();
+	console.log("Play");
+	document.querySelector("#volume").innerHTML = (video.volume * 100) + "%";
+
+
+});
+
+document.querySelector("#pause").addEventListener("click", function(){
+	video.pause();
+	console.log("Pause");
+	
+});
+
+document.querySelector("#slower").addEventListener("click", function(){
+	video.playbackRate * 0.95;
+	console.log("Slow speed down to" + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function(){
+	video.playbackRate * 1.05;
+	console.log("Increase speed by " + video.playbackRate);
+});
+
+
+document.querySelector("#skip").addEventListener("click", function(){
+	if(video.duration > video.currentTime + 15){
+		video.currentTime += 15;
+	
+	} else {
+		video.currentTime = 0;
+		console.log("Restart video at " + video.currentTime);
+	}
+	console.log(video.duration)
+	video.play();
+});
+
+document.querySelector("#mute").addEventListener("click", function(){
+	console.log("Mute Video");
+	if (video.muted == false){
+		video.muted = true
+		document.querySelector("#mute").innerHTML = "Unmute"
+	} else {
+			video.muted = false
+			document.querySelector("#mute").innerHTML = "Mute"		
+	}
+	console.log("Video is muted")
+});
+
+
+document.querySelector("#vintage").addEventListener("click", function(){
+	console.log("Set video to vintage mode");
+	video.classList.add("oldSchool")
+});
+
+document.querySelector("#orig").addEventListener("click", function(){
+	console.log("Set video back to original");
+	video.classList.remove("oldSchool")
+});
+
+document.querySelector("#slider").addEventListener("change", function(){
+	console.log(video.volume);
+	document.querySelector("#volume").innerHTML = this.value + "%";
+	video.volume = (this.value/100);
+});
+
 
